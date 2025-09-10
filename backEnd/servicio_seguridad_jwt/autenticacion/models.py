09b716e7517da7858c5385f19a19fdc5e86bff9a
Mónica Castellanos Méndez <monicacastellanos@unicauca.edu.co>
2025-09-09 21:16:48 -0500

    Primera versión funcional
    End-point de login genera token y refresh  para administrador y asignador

---
+++ backEnd/servicio_seguridad_jwt/autenticacion/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Modelo personalizado de usuario que extiende el modelo base AbstractUser de Django
+# Permite agregar campos adicionales al sistema de autenticación, como el rol del usuario
+class User(AbstractUser):
+    # Definición de los roles disponibles en la plataforma
+    # Cada tupla contiene: (valor almacenado en la base de datos, etiqueta legible para el administrador)
+    ROLE_CHOICES = (
+        ('Asignador', 'Asignador'),         # Usuario encargado de asignar tareas o recursos
+        ('Administrador', 'Administrador'), # Usuario que Administra electivas
+    )
+
+    # Campo adicional que define el rol del usuario dentro de la aplicación
+    # Se almacena como texto, limitado a 20 caracteres, y debe coincidir con una de las opciones definidas en ROLE_CHOICES
+    # El valor por defecto es 'Estudiante'
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='Estudiante'
+    )
