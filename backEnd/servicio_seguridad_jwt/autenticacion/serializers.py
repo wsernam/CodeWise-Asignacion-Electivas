3933ed67f0fd745103fa2df6c9f8c5d7dc596c54
Mónica Alejandra Castellanos Méndez <monicacastellanos@unicauca.edu.co>
2025-09-20 16:04:05 -0500

    Microservicio de seguridad (#1)
    * Primera versión funcional
    
    End-point de login genera token y refresh  para administrador y asignador
    
    * Eliminado un rquirements.txt exterior
    
    Sobra
    
    * nanay
    
    * Pequeños ajustes
    
    * txt para la tester
    
    ---------
    
    Co-authored-by: AIQuira <166058744+AIQuira@users.noreply.github.com>

---
+++ backEnd/servicio_seguridad_jwt/autenticacion/serializers.py
@@ -0,0 +1,19 @@
+# Serializador personalizado que extiende el serializador base de SimpleJWT
+# Permite modificar el contenido del token JWT emitido al autenticar al usuario
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+  # Llama al método original para generar el token base (access y refresh)
+
+    def get_token(cls, user):
+        token = super().get_token(user)
+         # Agrega información adicional al token: el rol del usuario
+        # Esto permite que el frontend o cualquier consumidor del token
+        # pueda acceder al rol sin hacer una consulta adicional
+
+        token['role'] = user.role
+        # Retorna el token modificado con el nuevo claim personalizado
+
+        return token
