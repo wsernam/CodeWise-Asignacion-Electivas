5511b33a658942125cdbe88d61f47fa62edb58d9
ashlee <ashleecampaz@unicauca.edu.co>
2025-10-03 07:07:00 -0500

    feat:configuracion basica rabbit

---
+++ backEnd/gestion-formulario/utilidades/rabbitMQ_publicador.py
@@ -0,0 +1,25 @@
+import pika, json
+from django.conf import settings
+
+def publish_message(message: dict):
+    credentials = pika.PlainCredentials(settings.RABBITMQ_USER, settings.RABBITMQ_PASS)
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=settings.RABBITMQ_HOST,
+            port=settings.RABBITMQ_PORT,
+            credentials=credentials
+        )
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue=settings.RABBITMQ_QUEUE, durable=True)
+
+    # Convertir dict a JSON antes de enviar
+    channel.basic_publish(
+        exchange='',
+        routing_key=settings.RABBITMQ_QUEUE,
+        body=json.dumps(message),
+        properties=pika.BasicProperties(delivery_mode=2)
+    )
+
+    connection.close()
+
