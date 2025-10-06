8daee5797b0c0175c781cab0526e2509c7605eee
Mónica Castellanos Méndez <monicacastellanos@unicauca.edu.co>
2025-10-06 17:57:24 -0500

    Se añade el endpoint para ver el estado del formulario

--- backEnd/gestion-formulario/gestion_estado_formulario/urls.py
+++ backEnd/gestion-formulario/gestion_estado_formulario/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('estado-formulario/', views.get_formulario_estado, name='get_formulario_estado'),
     path('toggle-formulario/', views.toggle_formulario, name='toggle_formulario'),
 ]
