dce92f1aa0ccd8c9db9a760863cc27b4b14d4d16
ashlee <ashleecampaz@unicauca.edu.co>
2025-10-17 16:11:54 -0500

    aisle el microservicio de notificaciones

--- backEnd/gestion-formulario/gestion_estado_formulario/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestionEstadoFormularioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestion_estado_formulario'
--- backEnd/gestion-formulario/gestion_estado_formulario/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('toggle-formulario/', views.toggle_formulario, name='toggle_formulario'),
-]
